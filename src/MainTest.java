3e704d760ef41884d79719f6c041267d09b3ffe4
Tinh. Nguyen Thi - CMC Global DU3.1 <nttinh1@cmcglobal.vn>
2022-08-26 16:18:51 +0700

    fix conflict

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,6 +1,6 @@
 public class MainTest {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world a");
         System.out.println("aaa");
     }
 }
