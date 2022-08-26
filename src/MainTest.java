356593f1a48d4adc32e522915a6bb7d88ea11f08
Tinh. Nguyen Thi - CMC Global DU3.1 <nttinh1@cmcglobal.vn>
2022-08-26 16:15:09 +0700

    fix

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,10 +1,6 @@
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("hello world");
-<<<<<<< HEAD
         System.out.println("aa");
-=======
-        System.out.println("asdffhgh");
->>>>>>> dev
     }
 }
